85db2523a3b1ba30514992fd542ff82d1c69f9a3
Lhokam <corentin@cclaudel.fr>
2025-09-03 19:16:26 +0200

    add disk module

---
+++ disk/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+variable "name" {
+  description = "Nom du disque à créer"
+  type        = string
+}
+
+variable "pool" {
+  description = "Nom du pool libvirt"
+  type        = string
+  default     = "default"
+}
+
+variable "base_volume_id" {
+  description = "ID de l'image source utilisée comme base"
+  type        = string
+}
+
+variable "size" {
+  description = "Taille du disque en octets"
+  type        = number
+}
+
+locals {
+  size = var.size * 1024 * 1024 * 1024 
+}
+
+resource "libvirt_volume" "this" {
+  name           = var.name
+  pool           = var.pool
+  base_volume_id = var.base_volume_id
+  size           = local.size
+}
+
+output "id" {
+  description = "ID du disque créé"
+  value       = libvirt_volume.this.id
+}
